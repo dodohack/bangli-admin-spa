e9d1e929e44feff6c0a1891e463d6876d8043e60
Hualin Wu <hualin.wu@imgtec.com>
2016-07-20 17:50:31 +0100

    Copy paste ng2-file-upload example into images modual

--- app/attachment/images.ts
+++ app/attachment/images.ts
@@ -2,22 +2,21 @@
  *
  */
 
-import { Component } from '@angular/core';
+import { Component }                          from '@angular/core';
+import {FILE_UPLOAD_DIRECTIVES, FileUploader} from 'ng2-file-upload';
+
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 @Component({
-    templateUrl: 'app/attachment/images.html'
+    templateUrl: 'app/attachment/images.html',
+    directives: [FILE_UPLOAD_DIRECTIVES]
 })
 export class ImagesPage
 {
-    constructor() {}
+    public uploader:FileUploader = new FileUploader({url: URL});
+    public hasDropZoneOver:boolean = false;
 
-    /**
-     * I should manage to dump sth from input
-     * @param $sth
-     */
-    logInput(files)
-    {
-        console.log('logging input');
-        console.log(files);
+    public fileOverBase(e:any):void {
+        this.hasDropZoneOver = e;
     }
 }
