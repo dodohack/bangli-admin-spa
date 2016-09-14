6fe68f5e29a8f8d7e166844f3d8cad4eab81a906
Aries Wu <aries@bangli.uk>
2016-09-14 21:55:25 +0100

    Remove unused plugin

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,14 +1,12 @@
 var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
 module.exports = webpackMerge(commonConfig, {
-    devtool: 'source-map',
+    //devtool: 'source-map',
 
     output: {
         path: helpers.root('public'),
