905cf7625fcf81df7d3a92d7973f49ed02261614
Hualin Wu <hualin.wu@imgtec.com>
2016-09-15 18:02:55 +0100

    Upgraded angular2 from rc6 to 2.0.0

--- app/modules/setting/cms/tag.ts
+++ app/modules/setting/cms/tag.ts
@@ -3,8 +3,10 @@
  */
 import { Component }               from '@angular/core';
 import { Input, Output }           from '@angular/core';
+import { OnChanges }               from '@angular/core';
 import { EventEmitter }            from '@angular/core';
 import { ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectorRef }       from '@angular/core';
 
 import { Tag }               from "../../../models";
 import { Channel }           from "../../../models";
@@ -14,7 +16,7 @@ import { Channel }           from "../../../models";
     template: require('./tag.html'),
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CmsTag
+export class CmsTag extends OnChanges
 {
     @Input() tags: Tag[];
     @Input() channel: Channel;
@@ -27,7 +29,14 @@ export class CmsTag
     @Output() add  = new EventEmitter();
     @Output() remove = new EventEmitter();
 
+    constructor(private cd: ChangeDetectorRef) {}
+
     setTag(tag) {
         this.tag = tag;
+    }
+
+    ngOnChanges(changes: any):void {
+        console.error("WE can mark this as dirty to refresh the page", changes);
+        this.cd.markForCheck();
     }
 }
