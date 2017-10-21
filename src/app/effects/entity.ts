6a254445f325499fca3631c1ced51f0c4553e684
Aries Wu <aries@bangli.uk>
2017-10-21 22:21:20 +0100

    Add FIXME

--- src/app/effects/entity.ts
+++ src/app/effects/entity.ts
@@ -59,6 +59,7 @@ export class EntityEffects {
             .mergeMap(ret => {
                 let actions = [
                     // Delete draft(id=0) entity when save is succeed
+                    console.error("FIXME: Think a better way, not always deleting id 0 entity!");
                     new entity.DeleteEntity({etype: ret.etype, data: 0}),
                     new entity.SaveEntitySuccess({etype: ret.etype, data: ret.entity})
                 ];
