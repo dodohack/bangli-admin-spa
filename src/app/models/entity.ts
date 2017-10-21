c57d3481c33268808fc60fccdb3d3f4b55c66ec5
Aries Wu <aries@bangli.uk>
2017-10-21 23:05:30 +0100

    Rank topic from 0 to 5.0

--- src/app/models/entity.ts
+++ src/app/models/entity.ts
@@ -73,7 +73,7 @@ export const TOPIC_TYPES = [
 /**
  * Topic display ranking
  */
-export const TOPIC_RANKINGS = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+export const TOPIC_RANKINGS = [ 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50 ];
 
 /** ALL:
  *  entity status, corresponding to each entity table column: status */
