794e95fa5ad165d215db37c52306b3063854d072
Aries Wu <aries@bangli.uk>
2017-10-29 16:54:18 +0000

    Improve entity list page UI

--- src/app/modules/directives/list-filter-bar.ts
+++ src/app/modules/directives/list-filter-bar.ts
@@ -149,35 +149,6 @@ export class ListFilterBar implements OnInit, OnDestroy {
         }
     }
 
-    /**
-     * We can loop over the array returned by this function to greatly simplify
-     * the html template, this 4 elements represent the state and style of
-     * 'first', 'previous', 'next' and 'last' pages.
-     */
-    get pagers() {
-        return [
-            {
-                index: 1,
-                icon: 'fa-angle-double-left',
-                disabled: this.paginator.cur_page === 1
-            },
-            {
-                index: this.paginator.pre_page,
-                icon: 'fa-angle-left',
-                disabled: this.paginator.cur_page === this.paginator.pre_page
-            },
-            {
-                index: this.paginator.next_page,
-                icon: 'fa-angle-right',
-                disabled: this.paginator.cur_page === this.paginator.next_page
-            },
-            {
-                index: this.paginator.last_page,
-                icon: 'fa-angle-double-right',
-                disabled: this.paginator.cur_page === this.paginator.last_page
-            },
-        ];
-    }
 
     /**
      * Pagination url
@@ -188,5 +159,9 @@ export class ListFilterBar implements OnInit, OnDestroy {
                 '/page/' + page + '/status/' + this.status;
         else
             return '/' + this.slug + '/page/' + page + '/status/' + this.status;
+    }
+
+    pageEvent(event) {
+        this.router.navigate([this.pageUrl(event.pageIndex+1)], {queryParams: this.queryParams});
     }
 }
